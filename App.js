668dd31e92e2d84e223e82747eadd65dc012f319
Atte <attesnn@gmail.com>
2023-01-12 17:08:22 +0200

    test

---
+++ App.js
@@ -0,0 +1,16 @@
+function show (src) {
+  document.getElementById('image')
+      .style.display = "block"; 
+  document.getElementById('image').src = src;
+}
+
+  function show5 () {
+      document.getElementById('image')
+      .style.display = "none"; 
+}
+
+
+
+
+
+    
